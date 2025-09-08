422093ffae934a0020b7eb7633bd40e7ceca9def
mariajulianacardino <mariajulianacardino.scc@gmail.com>
2025-09-08 02:01:34 +0000

    done

---
+++ SeatWorkElevenTwo.java
@@ -0,0 +1,10 @@
+public class SeatWorkElevenTwo {
+      public static void main(String[] args) {
+        int[][]myNumbers = { {1, 4, 2}, {3, 6, 8}
+        };
+
+        myNumbers[1][2] = 9;
+
+        System.out.println(myNumbers[1][2]);
+    }
+}
